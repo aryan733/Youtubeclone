b616f9a9a80ac9c779b21f916f803d3cd25d6d17
Aryan <aryanchavan186@gmail.com>
2024-06-08 23:21:56 +0530

    auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,7 +22,7 @@ export const login = async (req,res) => {
 
                 res.status(200).json({ result: newUser, token });
             } catch (error) {
-                res.status(500).json({ mess: error });
+                res.status(500).json({ mess: "Something went wrong..." });
             }
         } else {
            
@@ -36,6 +36,6 @@ export const login = async (req,res) => {
             res.status(200).json({ result: existingUser, token });
         }
     } catch (error) {
-        res.status(500).json({ mess: error });
+        res.status(500).json({ mess: "Something went wrong..." });
     }
 };
