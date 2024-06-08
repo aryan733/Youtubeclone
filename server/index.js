c445e9a9513d8024b70e41ba6b1045052316e600
Aryan <aryanchavan186@gmail.com>
2024-06-08 23:12:21 +0530

    env

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+  }))
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
