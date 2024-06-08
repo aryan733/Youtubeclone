1f3eae1b33fa6c7b0acc750f0f63bf6d7c6d38bd
Aryan <aryanchavan186@gmail.com>
2024-06-08 22:36:08 +0530

    server

--- server/index.js
+++ server/index.js
@@ -12,9 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: "*"
-  }))
+app.use(cors());
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
