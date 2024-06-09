62cf6ec97882b1a4a686eee8aba28d162e3eb6e9
Aryan <aryanchavan186@gmail.com>
2024-06-09 21:52:05 +0530

    CHannel

--- client/src/Reducers/channel.js
+++ client/src/Reducers/channel.js
@@ -1,10 +1,10 @@
-import { MdPendingActions } from "react-icons/md";
-
 const channelReducers = (state = [], action) => {
     switch (action.type) {
-        case 'UPDATE_DATA':
+        case 'UPDATE_DATA_SUCCESS':
+            // Assuming the payload contains the updated channel data
             return state.map(stateItem => stateItem._id === action.payload._id ? action.payload : stateItem);
-        case "FETCH_CHANNELS":
+        case 'FETCH_CHANNELS_SUCCESS':
+            // Assuming the payload contains the fetched channel data
             return action.payload;
         default:
             return state;
