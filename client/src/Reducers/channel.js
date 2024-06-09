d33bebd1adc6b5e5838cff2a6d9e3a9270788e04
Aryan <aryanchavan186@gmail.com>
2024-06-09 08:03:32 +0530

    channel

--- client/src/Reducers/channel.js
+++ client/src/Reducers/channel.js
@@ -1,13 +1,14 @@
 import { MdPendingActions } from "react-icons/md";
 
-const channelReducers =(states=[],action)=>{
-    switch(action.type){
+const channelReducers = (state = [], action) => {
+    switch (action.type) {
         case 'UPDATE_DATA':
-            return states.map(state=>state._id=== MdPendingActions.payload._id? action.payload: state)
+            return state.map(stateItem => stateItem._id === action.payload._id ? action.payload : stateItem);
         case "FETCH_CHANNELS":
-                  return action.payload;
-            default:
-                return states;
+            return action.payload;
+        default:
+            return state;
     }
-}
+};
+
 export default channelReducers;
