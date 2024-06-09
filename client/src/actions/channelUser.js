21dd9a31d60c6de6e8d24f7cae15d2e1397b71aa
Aryan <aryanchavan186@gmail.com>
2024-06-09 08:11:23 +0530

    update

--- client/src/actions/channelUser.js
+++ client/src/actions/channelUser.js
@@ -1,19 +1,21 @@
 import * as api from '../api';
 
-
-export const fetchAllChannel=()=>async(dispatch)=>{
-    try{
-        const {data}= await api.fetchAllChannel();
-        dispatch({type:'FETCH_CHANNELS',payload:data})
-    }catch(error){
-        console.log(error)
+export const fetchAllChannel = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchAllChannel();
+        dispatch({ type: 'FETCH_CHANNELS_SUCCESS', payload: data });
+    } catch (error) {
+        console.log(error);
+        dispatch({ type: 'FETCH_CHANNELS_FAILURE', payload: error.message });
     }
-}
-export const updateChannelDate=(id,updateData)=> async(dispatch)=>{
-try {
-    const {data}=await api.updateChannelData(id,updateData);
-    dispatch({type:'UPDATE_DATA',payload: data})
-} catch (error) {
-    console.log(error)
-} 
-}
+};
+
+export const updateChannelData = (id, updateData) => async (dispatch) => {
+    try {
+        const { data } = await api.updateChannelData(id, updateData);
+        dispatch({ type: 'UPDATE_DATA_SUCCESS', payload: data });
+    } catch (error) {
+        console.log(error);
+        dispatch({ type: 'UPDATE_DATA_FAILURE', payload: error.message });
+    }
+};
